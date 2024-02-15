e356129707c70305cff27e017e8f518ba23061aa
Aarju <aarjuelectrical@gmail.com>
2024-02-15 23:43:14 +0530

    Added components and pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+import NotFound from "./NotFound";
+
+export { About, Contact, Home, Login, Register, NotFound };
